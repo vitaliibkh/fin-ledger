0a70f6e65ca9c772d8729370ebf3195d688e87b5
vitaliibkh <vitalii.bakhurskyi@gmail.com>
2026-03-30 12:24:24 +0300

    Cleanup

--- frontend/fin-ledger/src/app/app.ts
+++ frontend/fin-ledger/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('FinLedger');
+  protected readonly title = signal('fin-ledger');
 }
